4c73f0d1ca66d8a8c9448d80548f6b18cb9a774f
190968 <19ham09@gmail.com>
2025-11-03 11:47:36 +0300

    first_commit

--- main.py
+++ main.py
@@ -4,91 +4,108 @@ import sys
 import time
 from datetime import datetime
 
+import requests
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.methods.get_my_name import GetMyName
+import aiogram.types.chat
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+# from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram import Bot, Dispatcher, types, html, Router
 from aiogram import F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters.command import Command
+from aiogram.filters.command import Command, CommandStart
 from aiogram_broadcaster import Broadcaster
-from aiogram_broadcaster.contents import MessageSendContent
 from aiogram_broadcaster.storages.file import FileMailerStorage
 from handlers import help, my_main, coord, photo, exchange, broad
 
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = '6700779699:AAGw3dk2xDbj0-ThSiJjWukhxcBkapYmUY0'
+from config import TOKEN
 
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher()
-dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
-dp["developer"] = "Ivanopulo and ZUZ"
-# chat_ids = []
+
+dp = Dispatcher(bot=Bot, storage=MemoryStorage())
+dp["started_at"] = time.strftime('%d/%b/%Y %H:%M')
+dp["developer"] = "Ivanopulo and Zuz"
 user_ids = {}
 
 
-@dp.message(Command("test"))
+class CurrencyForm(StatesGroup):
+    count = State()
+    like_bots = State()
+    language = State()
+
+
+mylist = set()
+
+
+@dp.message(F.text.startswith('test'))
 async def cmd_test1(message: types.Message):
+    bot_name = await bot(GetMyName())
+    print(bot_name)
+    mylist.add(message.from_user.first_name)
+    for z in mylist:
+        print(z)
     info_chat = message.from_user.language_code
-    print(info_chat)
-    await message.answer_dice(emoji="🎲", content=info_chat)
+    await message.answer(f"Hello {message.from_user.first_name}. My {bot_name}. Test is good")
 
-
-
-
+@dp.message(F.text.startswith('message'))
+async def webhook(message: types.Message):
+    bot_name = await bot(GetMyName())
+    response = requests.post('https://bobozeranski.app.n8n.cloud/webhook-test/APIsssi', json={'message': message.from_user.first_name})
+    data = response.json()
+    my_message = data.get('message', 'No message found')
+    print(my_message)
+    await message.answer(f"Hello {message.from_user.first_name}. My {my_message}. Test is good")
 
 @dp.message(Command("info"))
 async def info(message: types.Message, started_at: str, developer: str):
-    await message.answer(f"Bot started {started_at} , developer : {developer} ")
+    await message.answer(f"Bot started in {started_at} .\nDevelopers : {developer}. ")
 
+
+@dp.message(Command("send_span"))
+async def send(message: types.Message):
+    print(message.date, message.from_user.first_name, mylist)
+    await message.answer('czxczxcxzczxc')
 
 @dp.message(Command("list"))
 async def cmd_test1(message: types.Message, mylist: set[str]):
     mylist.add(message.from_user.first_name)
-    await message.answer(f" Users the bot: <b> {', '.join(mylist)} </b>", parse_mode=ParseMode.HTML)
+    print(message.date, message.from_user.first_name)
+    await message.answer(f" The users this bot: <b> {', '.join(mylist)} </b>",
 
+                         parse_mode=ParseMode.HTML)
 
-@dp.message(Command("start"))
+@dp.message(Command("buy"))
+async def buy_shoes(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Нажми меня",
+        callback_data="random_value")
+    )
+    await message.answer(f"Привет мой друг <b>{html.bold(message.from_user.full_name)}!</b>\n Хочешь купить валюту?",
+                         reply_markup=builder.as_markup(),
+                         parse_mode=ParseMode.HTML)
+@dp.message(CommandStart())
 async def send_welcome(message: types.Message):
-    # chat_ids.append(message.chat.id)
     f = open("id.txt", "a")
     f.write(str(message.chat.id) + ',')
-    await message.answer(f"Привет мой друг ,{html.bold(message.from_user.full_name)}! "
-                         f"\nИспользуй /help, чтобы узнать список доступных команд!")
+    await message.answer(f"Привет мой друг  <b>{html.bold(message.from_user.full_name)}!</b> "
+                         f"\n Используй команду  /help , чтобы узнать список доступных команд!",
+                         parse_mode=ParseMode.HTML)
 
 
-@dp.message(Command('messaging'))
-async def periodic(message: types.Message):
-    # loop = asyncio.get_running_loop()
-    # loop.call_later(10, periodic, message)
-    await asyncio.sleep(10)
-    await bot.send_message(message.from_user.id, f"Hello ", disable_notification=True)
+@dp.message(Command('setstate'))
+async def periodic(message: types.Message, state: FSMContext):
+    await state.set_state(CurrencyForm.count)
+
+    await bot.send_message(message.from_user.id, f"Hello, enter count ", disable_notification=True)
 
 
-@dp.message(F.text.lower() == "good")
-async def good(message: types.Message):
-    await message.reply("Yes. All right and well.")
-
-
-@dp.message(F.text.lower() == "отличное")
-async def with_puree(message: types.Message):
-    await message.reply("Рад за тебя !")
-
-
-@dp.message(F.text.lower() == "среднее")
-async def without_puree(message: types.Message):
-    await message.reply("Надейся на лучшее !")
-
-
-@dp.message(F.text.lower() == "плохое")
-async def without_puree(message: types.Message):
-    await message.reply("Иди бухни чувак !")
-
-
-current_time = time.strftime('%A %B, %d %Y %H:%M:%S')
-
-
-def later():
-    print('bcvbcv')
+current_time = time.strftime('%d/%b/%Y %H:%M')
 
 
 async def main():
